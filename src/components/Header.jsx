d30bc90554baa17c444dbfb72255ee87e1d75b29
Bless <ig.marks@yandex.ru>
2024-05-01 20:11:24 +0300

    prepair_project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import styled from 'styled-components';
-import {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import styled from 'styled-components'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 
-import { Container } from './Container';
+import { Container } from './Container'
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
   background-color: var(--colors-ui-base);
-`;
+`
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 2rem 0;
-`;
+`
 
 const Title = styled(Link).attrs({
   to: '/',
@@ -24,7 +24,7 @@ const Title = styled(Link).attrs({
   font-size: var(--fs-sm);
   text-decoration: none;
   font-weight: var(--fw-bold);
-`;
+`
 
 const ModeSwitcher = styled.div`
   color: var(--colors-text);
@@ -32,14 +32,14 @@ const ModeSwitcher = styled.div`
   cursor: pointer;
   // font-weight: var(--fw-bold);
   text-transform: capitalize;
-`;
+`
 
 export const Header = () => {
-  const theme = 'light';
+  const theme = 'dark'
 
   useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-  }, [theme]);
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
 
   return (
     <HeaderEl>
@@ -57,5 +57,5 @@ export const Header = () => {
         </Wrapper>
       </Container>
     </HeaderEl>
-  );
-};
+  )
+}
